4ca0c422d397eb28f87cc0e99323bde29c1ecaa1
邱勇嘉 <d1204429@o365.fcu.edu.tw>
2024-08-07 10:16:54 +0800

    實作Toast

--- ListViewTest/app/src/main/java/com/example/listviewtest/MainActivity.java
+++ ListViewTest/app/src/main/java/com/example/listviewtest/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.listviewtest;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +13,9 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,23 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        //綁定 註冊Listener
+        ListView lv = (ListView) findViewById(R.id.cityListView);
+        lv.setOnItemClickListener(this);
+    }
+
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        TextView tv = (TextView) findViewById(R.id.textView);
+        String[] citiesArray = getResources().getStringArray(R.array.cities);
+        Toast.makeText(
+                this,
+                "您選擇的是 : " + citiesArray[i],
+                Toast.LENGTH_SHORT
+        ).show();
+
+//        tv.setText("您選擇的是 : " + citiesArray[i]);
+
     }
 }
