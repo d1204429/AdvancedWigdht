b80cef395d14fe6be9ea88c8e5b5e4e97c81be6e
邱勇嘉 <d1204429@o365.fcu.edu.tw>
2024-08-07 11:17:40 +0800

    透過自訂ArrayAdapter的方式，讓ListView的內容能自動變化，修正BUG

--- ListViewTest/app/src/main/java/com/example/listviewtest/MainActivity.java
+++ ListViewTest/app/src/main/java/com/example/listviewtest/MainActivity.java
@@ -15,8 +15,6 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import org.w3c.dom.Text;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -24,61 +22,56 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
     private ListView cityListView;
-    private List<String> cities = new ArrayList<String>();
-
+    private List<String> cities = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        // 設置窗口邊距
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        //綁定 註冊Listener
-        ListView lv = (ListView) findViewById(R.id.cityListView);
+
+        // 綁定 ListView 並註冊 Listener
+        cityListView = findViewById(R.id.cityListView);
         setCities();
         cityListView.setOnItemClickListener(this);
-//        lv.setOnItemClickListener(this);
     }
 
-    //動態新增城市
-    private void setCities(){
+    // 動態新增城市
+    private void setCities() {
         cities = new ArrayList<>(Arrays.asList(getResources().getStringArray(R.array.cities)));
         cities.add("Hualien");
         cities.add("Taitung");
         cities.add("Changhua");
-        //new ArrayAdapter
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,cities);
+
+        // new ArrayAdapter
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, cities);
         cityListView.setAdapter(adapter);
     }
 
     @Override
     public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-        TextView tv = (TextView) findViewById(R.id.textView);
-        String[] citiesArray = getResources().getStringArray(R.array.cities);
+        String selectedCity = cities.get(i);
         AlertDialog.Builder dialog = new AlertDialog.Builder(this);
-        dialog.setTitle("城市").setMessage("您選擇的是 : " + citiesArray[i]).setCancelable(true).show();
-        dialog.setPositiveButton("確定",null);
-        dialog.setNegativeButton("取消",null);
-        dialog.setNegativeButton("放棄",null);
-        dialog.show();
+        dialog.setTitle("城市")
+                .setMessage("您選擇的是 : " + selectedCity)
+                .setCancelable(true)
+                .setPositiveButton("確定", null)
+                .setNegativeButton("取消", null)
+                .setNeutralButton("放棄", null)
+                .show();
 
+        // 顯示 Toast 信息（如果需要）
+        // Toast.makeText(this, "您選擇的是 : " + selectedCity, Toast.LENGTH_SHORT).show();
 
-//        dialog.setTitle("城市");
-//        dialog.setMessage("您選擇的是 : " + citiesArray[i]);
-//        dialog.setCancelable(true);
-//        dialog.show();
-
-//        Toast.makeText(
-//                this,
-//                "您選擇的是 : " + citiesArray[i],
-//                Toast.LENGTH_SHORT
-//        ).show();
-
-//        tv.setText("您選擇的是 : " + citiesArray[i]);
-
+        // 更新 TextView 信息（如果需要）
+        // TextView tv = findViewById(R.id.textView);
+        // tv.setText("您選擇的是 : " + selectedCity);
     }
 }
